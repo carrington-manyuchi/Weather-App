f19352cbffa3f925a1887407dde6dc137f009f23
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2026-09-19 09:18:12 +0200

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Manyuchi, Carrington C on 2026/09/19.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
